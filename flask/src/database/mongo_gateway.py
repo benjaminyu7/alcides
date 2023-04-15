44a28f0a4685ade4bbebab7942ef017e1374e458
Benjamin Yu <benjamin.yu741@gmail.com>
2023-04-15 16:54:58 -0700

    Merge

--- flask/src/database/mongo_gateway.py
+++ flask/src/database/mongo_gateway.py
@@ -7,13 +7,12 @@ class MongoGateway:
     def __init__(self, mongo_client):
         pass
 
-    def get(self, database, collection, dictionary):
+    def get(self, database: str, collection: str, dictionary:dict):
         database = self.mongo_client[database]
         collection = database[collection]
         return collection.find_one(dictionary)
 
-    # 
-    def create_or_update_list(self, database, collection, query, pushListElementQuery):
+    def create_or_update_list(self, database: str, collection: str, query: dict, pushListElementQuery: dict):
         database = self.mongo_client[database]
         collection = database[collection]
         collection.update_one(query, {"$push":pushListElementQuery}, upsert=True)
